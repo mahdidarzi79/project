40a6d9d6d5553515413764730966d402d102885f
mahdi <darzim97@gmail.com>
2018-08-13 18:35:35 +0430

    add content folder project

--- src/components/lightbox2.js
+++ src/components/lightbox2.js
@@ -11,6 +11,9 @@ const {width:devicewidth ,height:deviceheight}=Dimensions.get('window');
     _renderLightbox() {
         // const {children}=this.propsss;
        // const {children}=this.propsss;
+       // const {children}=this.propsss;// const {children}=this.propsssas;
+       //sgit 
+       
         return (
             <View style={{ width:300 , height:300, justifyContent: 'center' , alignItems: 'center' , backgroundColor : 'white' , borderRadius : 4}}>
 
